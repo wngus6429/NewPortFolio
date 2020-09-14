821dac09c56ee1d67f2b2af9d0347720e3c7623f
wngus6429 <55697824+wngus6429@users.noreply.github.com>
2020-09-14 22:02:32 +0900

    scroll
    scroll

--- main.js
+++ main.js
@@ -10,3 +10,17 @@ document.addEventListener("scroll", () => {
     navbar.classList.remove("navbar--dark");
   }
 });
+
+// Handle Scrolling when tapping on the navbar menu
+
+const navbarMenu = document.querySelector(".navbar__menu");
+navbarMenu.addEventListener("click", () => {
+  //   data-link="#about 이런식으로 달아준거 dataset안에 들어있따.
+  const link = event.target.dataset.link;
+  if (link == null) {
+    return;
+  }
+  console.log(event.target.dataset.link);
+  const scrollTo = document.querySelector(link);
+  scrollTo.scrollIntoView({ behavior: "smooth" });
+});
