e48de96da96933b335aa2614024ba3f0a4eb8ee8
wngus6429 <55697824+wngus6429@users.noreply.github.com>
2020-09-14 22:07:16 +0900

    914
    914

--- main.js
+++ main.js
@@ -3,3 +3,29 @@
 hello;
 GainNode;
 gameboold;
+/** @format */
+
+// Make navbar transpaarent when it is on the top
+const navbar = document.querySelector("#navbar");
+const navbarHeight = navbar.getBoundingClientRect().height;
+document.addEventListener("scroll", () => {
+  if (window.scrollY > navbarHeight) {
+    navbar.classList.add("navbar--dark");
+  } else {
+    navbar.classList.remove("navbar--dark");
+  }
+});
+
+// Handle Scrolling when tapping on the navbar menu
+
+const navbarMenu = document.querySelector(".navbar__menu");
+navbarMenu.addEventListener("click", () => {
+  //   data-link="#about 이런식으로 달아준거 dataset안에 들어있따.
+  const link = event.target.dataset.link;
+  if (link == null) {
+    return;
+  }
+  console.log(event.target.dataset.link);
+  const scrollTo = document.querySelector(link);
+  scrollTo.scrollIntoView({ behavior: "smooth" });
+});
