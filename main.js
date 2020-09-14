c4f94ab67f04d36e4fcc6be05d2b2285613cdf82
wngus6429 <55697824+wngus6429@users.noreply.github.com>
2020-09-14 21:33:50 +0900

    914
    914

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
 /** @format */
 
-hello;
-GainNode;
-gameboold;
+// Make navbar transpaarent when it is on the top
+const navbar = document.querySelector("#navbar");
+const navbarHeight = navbar.getBoundingClientRect().height;
+document.addEventListener("scroll", () => {
+  if (window.scrollY > navbarHeight) {
+    navbar.classList.add("navbar--dark");
+  } else {
+    navbar.classList.remove("navbar--dark");
+  }
+});
